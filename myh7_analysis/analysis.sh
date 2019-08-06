0a264e01515bb718d30414d68c9fb8dd4a95736a
Rubakan Thurupan <rubakan.thurupan@nmbu.no>
2019-08-06 11:09:44 +0100

    Initial version

--- myh7_analysis/analysis.sh
+++ myh7_analysis/analysis.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./ExtractExcelMutations.py MYH7_Luis_Lopes.xlsx 'Majid MYH7' MYH7protein P12883 | sort -u > myh7.mut
+./ExtractExcelMutations.py 2013_MYH7_Luis_Lopes.xlsx 'Ruba MYH7' MYH7protein P12883 | sort -u > mutations_edited2019.mut
